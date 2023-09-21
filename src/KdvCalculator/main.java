8b1f998d9c07845f751e65db4f6f3fdaf349ee0c
mertokumusoglu <mertokumusoglu4@gmail.com>
2023-09-21 12:36:21 +0300

    Second Practice

--- src/KdvCalculator/main.java
+++ src/KdvCalculator/main.java
@@ -1,7 +1,18 @@
 package KdvCalculator;
 
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        System.out.print("Hello, im a second practice.");
+        double kdv, price_with_kdv;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Please enter a price: ");
+        double price = sc.nextDouble();
+
+        kdv = (price >= 1000) ? 0.18 : 0.8;
+        price_with_kdv = price + (price * kdv);
+
+        System.out.println("Price with kdv is: " + price_with_kdv);
+        System.out.println("Price of kdv is: " + kdv);
+        }
     }
-}
