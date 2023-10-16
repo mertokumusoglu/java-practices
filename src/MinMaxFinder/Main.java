49af859bdf7658221a722a435a8be7ad80590448
mertokumusoglu <mertokumusoglu4@gmail.com>
2023-10-16 17:57:40 +0300

    27th (edited)

--- src/MinMaxFinder/Main.java
+++ src/MinMaxFinder/Main.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int max = 0,min = 0;
+        int max = Integer.MIN_VALUE,min = Integer.MAX_VALUE;
 
         System.out.print("Kaç tane sayı gireceksiniz: ");
         int n = sc.nextInt();
         while(n > 0) {
             for(int i  = 1; i <= n; i++) {
-                System.out.print(i +". Sayıyı giriniz: ");
+                System.out.print(i + ". Sayıyı giriniz: ");
                 int m = sc.nextInt();
 
                 if(m > max) {
                     max = m;
                 }
-                else if (min < n) {
+                if (m < min) {
                     min = m;
                 }
             }
